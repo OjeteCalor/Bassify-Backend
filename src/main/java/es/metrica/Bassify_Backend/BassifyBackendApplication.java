a61db7eb8b207e83e791ff4a63685b6f727185b5
JaviC <ojetecalormetrica@gmail.com>
2025-05-22 14:09:07 +0200

    Primer commit

---
+++ src/main/java/es/metrica/Bassify_Backend/BassifyBackendApplication.java
@@ -0,0 +1,13 @@
+package es.metrica.Bassify_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BassifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BassifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/metrica/Bassify_Backend/BassifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.metrica.Bassify_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BassifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
