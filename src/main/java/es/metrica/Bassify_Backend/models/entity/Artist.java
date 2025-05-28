bc121d269182b247d9ab2ba031c978fbe4fc6876
AntonioRG <ojetecalormetrica@gmail.com>
2025-05-28 15:15:07 +0200

    Me quedan 17 dedos

--- src/main/java/es/metrica/Bassify_Backend/models/entity/Artist.java
+++ src/main/java/es/metrica/Bassify_Backend/models/entity/Artist.java
@@ -1,8 +1,50 @@
 package es.metrica.Bassify_Backend.models.entity;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "artist")
 public class Artist {
-	// TODO
+	@Id
+	@Column(name = "id", length = 30)
+	private String id;
+	
+	@Column(name = "artist_name", length = 100)
+	private String name;
+	
+	@Column(name = "genres")
+	private List<String> genres;
+	
+	public Artist() {}
+
+	public String getSpotifyId() {
+		return id;
+	}
+
+	public void setSpotifyId(String spotifyId) {
+		this.id = spotifyId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getGenres() {
+		return genres;
+	}
+
+	public void setGenres(List<String> genres) {
+		this.genres = genres;
+	}
+	
+	
 }
