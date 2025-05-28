11ae34fd6f407a0fe3204a99e91f4210d8e489d1
JaviC <ojetecalormetrica@gmail.com>
2025-05-28 11:31:58 +0200

    Actualización de la estructura

--- src/main/java/es/metrica/Bassify_Backend/controllers/UserController.java
+++ src/main/java/es/metrica/Bassify_Backend/controllers/UserController.java
@@ -1,5 +1,25 @@
 package es.metrica.Bassify_Backend.controllers;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import es.metrica.Bassify_Backend.models.Track;
+
+@Controller
+@RequestMapping("/api/user")
 public class UserController {
 
+	@GetMapping("/login")
+	@ResponseBody
+	public ResponseEntity<Track> startSession(@RequestBody Requ){
+		return null;
+	}
+	
 }
