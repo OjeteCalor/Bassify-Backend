541004006e07842f77367ce2cccb267bd02f637b
Gabriel <gabriel.gonzalez37h@gmail.com>
2025-05-29 15:14:01 +0200

    Logica del algoritmo

--- src/main/java/es/metrica/Bassify_Backend/models/logic/Algorithm.java
+++ src/main/java/es/metrica/Bassify_Backend/models/logic/Algorithm.java
@@ -1,5 +1,29 @@
 package es.metrica.Bassify_Backend.models.logic;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import es.metrica.Bassify_Backend.models.entity.Track;
+import es.metrica.Bassify_Backend.models.entity.WeightedPreference;
+
 public class Algorithm {
-	// TODO
+	private static final int DEFAULT_QUANTITY = 20;
+	
+	public static List<Track> getTracks(Set<WeightedPreference> preferences){
+		return getTracks(preferences, DEFAULT_QUANTITY);
+	}
+	
+	public static List<Track> getTracks(Set<WeightedPreference> preferences, int quantity){
+		List<Track> tracks = new LinkedList<>();
+		for(WeightedPreference pref : preferences) {
+			tracks.addAll(new TrackRequest().getTrackResponse(
+					pref.getGenre(),
+					pref.getLikedSongsCount().intValue() / pref.getListenedSongCount().intValue(),
+					pref.getListenedSongCount().intValue()
+				));
+		}
+		return tracks;
+	}
 }
+
