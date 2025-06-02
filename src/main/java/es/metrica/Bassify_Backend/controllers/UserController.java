ea824786dde0eb5262758936d8a10f524b7158db
JaviC <ojetecalormetrica@gmail.com>
2025-06-02 09:35:45 +0200

    Arreglo de la comunicación en el login del Usuario

--- src/main/java/es/metrica/Bassify_Backend/controllers/UserController.java
+++ src/main/java/es/metrica/Bassify_Backend/controllers/UserController.java
@@ -3,12 +3,14 @@ package es.metrica.Bassify_Backend.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import es.metrica.Bassify_Backend.models.dto.UserDTO;
 import es.metrica.Bassify_Backend.services.UserService;
@@ -17,25 +19,26 @@ import es.metrica.Bassify_Backend.services.UserService;
 @RequestMapping("/api/v1/account")
 public class UserController {
 	
-	private final UserService userService;
+	@Autowired
+	private UserService userService;
 
-	public UserController(UserService userService) {
-		this.userService = userService;
-	}
-	
 	@GetMapping("/login")
-	public ResponseEntity<UserDTO> login(@RequestBody UserDTO userDto){
-		return userService.login(userDto);
+	public Map<String, Object> login(@RequestBody UserDTO userDto){
+//		return map; userService.login(userDto);
+		ResponseEntity<UserDTO> responseDto = userService.login(userDto);
+		Map<String, Object> map = new HashMap<>();
+		map.put("status", responseDto.getStatusCodeValue());
+		map.put("is_new", responseDto.getStatusCode() == HttpStatus.CREATED);
+		map.put("created_user", responseDto.getBody());
+		return map;
 	}
 	
 	@PostMapping("/create/preferences")
 	public Map<String, Object> createPreferences(@RequestBody UserDTO userDto){
 		// return userService.createPreference(userDto);
 		ResponseEntity<UserDTO> responseDto = userService.createPreference(userDto);
-		Map<String, Object> map = new HashMap<>();
+		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("status", responseDto.getStatusCodeValue());
-		map.put("is_new", responseDto.getStatusCode() == HttpStatus.CREATED);
-		map.put("created_user", responseDto.getBody());
 		return map;
 	}
 	
