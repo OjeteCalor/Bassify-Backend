9d30a3847f4de65e266a9a50f629cb49561e1303
Gabriel <gabriel.gonzalez37h@gmail.com>
2025-06-03 08:56:25 +0200

    ArtistDTO vacio

---
+++ src/main/java/es/metrica/Bassify_Backend/models/dto/ArtistDTO.java
@@ -0,0 +1,10 @@
+package es.metrica.Bassify_Backend.models.dto;
+
+import es.metrica.Bassify_Backend.models.entity.Artist;
+
+public class ArtistDTO {
+
+	public ArtistDTO(Artist a) {
+		// TODO Auto-generated constructor stub
+	}
+}
