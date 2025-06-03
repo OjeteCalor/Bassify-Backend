669ecb9b90ae38be1dadc946cd7542c942e29594
AntonioRG <ojetecalormetrica@gmail.com>
2025-06-03 10:51:20 +0200

    Properties convertido a singleton

---
+++ src/main/java/es/metrica/Bassify_Backend/properties/PropertiesSingleton.java
@@ -0,0 +1,24 @@
+package es.metrica.Bassify_Backend.properties;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import es.metrica.Bassify_Backend.exceptions.BassifyException;
+import es.metrica.Bassify_Backend.models.logic.toolbox.AccesToken;
+
+public class PropertiesSingleton {
+	public static Properties p;
+	public static Properties getProperties() throws BassifyException {
+		if(p == null) {
+			p = new Properties();
+			p = new Properties();
+			try(InputStream input = AccesToken.class.getClassLoader().getResourceAsStream("accestoken.properties")){
+				p.load(input);
+			} catch (IOException e) {
+				throw new BassifyException("Properties file is corrupted or is missing");
+			}
+		}
+		return p;
+	}
+}
