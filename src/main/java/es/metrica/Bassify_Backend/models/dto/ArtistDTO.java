3eb100b2ebd0d701308dc65587517bb56b02e3c0
JaviC <ojetecalormetrica@gmail.com>
2025-06-03 08:48:14 +0200

    Creación de clase ArtistDTO y modificación en la entidad

---
+++ src/main/java/es/metrica/Bassify_Backend/models/dto/ArtistDTO.java
@@ -0,0 +1,30 @@
+package es.metrica.Bassify_Backend.models.dto;
+
+import es.metrica.Bassify_Backend.models.entity.Artist;
+
+public class ArtistDTO {
+
+	private String artistSpotifyId;
+	private String artistName;
+	
+	public ArtistDTO() {}
+
+	public ArtistDTO(Artist artist) {
+		this.artistSpotifyId = artist.getArtistSpotifyId();
+		this.artistName = artist.getArtistName();
+	}
+	
+	public String getArtistSpotifyId() {
+		return artistSpotifyId;
+	}
+	public void setArtistSpotifyId(String artistSpotifyId) {
+		this.artistSpotifyId = artistSpotifyId;
+	}
+	public String getArtistName() {
+		return artistName;
+	}
+	public void setArtistName(String artistName) {
+		this.artistName = artistName;
+	}
+	
+}
