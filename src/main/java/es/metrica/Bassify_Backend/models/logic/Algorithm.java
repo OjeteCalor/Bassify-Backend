7767e60cf074044376a97c9fc3a85b97a0d9d4de
Gabriel <gabriel.gonzalez37h@gmail.com>
2025-06-03 13:23:19 +0200

    Apaño para que funcionen

--- src/main/java/es/metrica/Bassify_Backend/models/logic/Algorithm.java
+++ src/main/java/es/metrica/Bassify_Backend/models/logic/Algorithm.java
@@ -4,26 +4,28 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+import es.metrica.Bassify_Backend.models.dto.TrackDTO;
 import es.metrica.Bassify_Backend.models.entity.Track;
 import es.metrica.Bassify_Backend.models.entity.WeightedPreference;
 
 public class Algorithm {
 	private static final int DEFAULT_QUANTITY = 20;
 	
-	public static List<Track> getTracks(Set<WeightedPreference> preferences){
+	public static List<TrackDTO> getTracks(Set<WeightedPreference> preferences){
 		return getTracks(preferences, DEFAULT_QUANTITY);
 	}
 	
-	public static List<Track> getTracks(Set<WeightedPreference> preferences, int quantity){
-		List<Track> tracks = new LinkedList<>();
-		for(WeightedPreference pref : preferences) {
-			tracks.addAll(new TrackRequest().getTrackResponse(
-					pref.getGenre(),
-					pref.getLikedTracksCount().intValue() / pref.getListenedTracksCount().intValue(),
-					pref.getListenedTracksCount().intValue()
-				));
-		}
-		return tracks;
+	public static List<TrackDTO> getTracks(Set<WeightedPreference> preferences, int quantity){
+//		List<Track> tracks = new LinkedList<>();
+//		for(WeightedPreference pref : preferences) {
+//			tracks.addAll(new TrackRequest().getTrackResponse(
+//					pref.getGenre(),
+//					pref.getLikedTracksCount().intValue() / pref.getListenedTracksCount().intValue(),
+//					pref.getListenedTracksCount().intValue()
+//				));
+//		}
+//		return tracks;
+		return null;
 	}
 }
 
--- src/main/java/es/metrica/Bassify_Backend/models/logic/TrackRequest.java
+++ src/main/java/es/metrica/Bassify_Backend/models/logic/TrackRequest.java
@@ -1,5 +1,6 @@
 package es.metrica.Bassify_Backend.models.logic;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,6 +9,8 @@ import java.util.Map;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,25 +18,27 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import es.metrica.Bassify_Backend.models.dto.TrackDTO;
 import es.metrica.Bassify_Backend.models.entity.Track;
 import es.metrica.Bassify_Backend.models.logic.toolbox.AccesToken;
 import es.metrica.Bassify_Backend.models.logic.toolbox.JsonParser;
+import es.metrica.Bassify_Backend.properties.PropertiesSingleton;
 
 public class TrackRequest {
 	
 	private static final String API_REQUEST = "https://api.spotify.com/v1/search?q={query}&type={type}&limit={limit}&offset={offset}";
 	
-	List<Track> getTrackResponse(String genre){
+	List<TrackDTO> getTrackResponse(String genre){
 		return getTrackResponse(genre, 0);
 	}
 	
-	List<Track> getTrackResponse(String genre, int limit){
+	List<TrackDTO> getTrackResponse(String genre, int limit){
 		return getTrackResponse(genre, limit, 0);
 	}
 	
-	List<Track> getTrackResponse(String genre, int limit, int offset){
+	List<TrackDTO> getTrackResponse(String genre, int limit, int offset){
 		RestTemplate restTemplate = new RestTemplate();
-		String accessToken = AccesToken.getAccessToken();
+		String accessToken = AccesToken.getAccessToken(PropertiesSingleton.getProperties().getProperty("refreshToken"));
         String url = API_REQUEST;
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", accessToken);
@@ -44,26 +49,17 @@ public class TrackRequest {
         params.put("type", "track");
         params.put("limit", String.valueOf(limit));
         params.put("offset", String.valueOf(offset));
-        
-        ResponseEntity<String> response = restTemplate.exchange(
+      
+        ResponseEntity<TrackDTO[]> response = restTemplate.exchange(
                 url,
                 HttpMethod.GET,
                 entity,
-                String.class,
+                TrackDTO[].class,
                 params
         );
+
         
-        List<Track> tracks = new LinkedList<>();
-        
-        if (!response.getStatusCode().isError()) {
-        	try {
-				JsonNode json = new ObjectMapper().readTree(response.getBody());
-				tracks.add(JsonParser.parseTrack(json));
-			} catch (JsonProcessingException e) {
-				e.printStackTrace();
-			}
-        }
-        
-        return tracks;
+//        return response.getBody();
+        return null;
 	}
 }
