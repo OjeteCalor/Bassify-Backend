1da6d7a154494aeaa1b925843564c86a7758097b
AntonioRG <ojetecalormetrica@gmail.com>
2025-06-04 11:35:32 +0200

    Ahora se usa DTO en algorithm

---
+++ src/main/java/es/metrica/Bassify_Backend/models/dto/WeightedPreferenceDTO.java
@@ -0,0 +1,43 @@
+package es.metrica.Bassify_Backend.models.dto;
+
+import es.metrica.Bassify_Backend.models.entity.WeightedPreference;
+
+public class WeightedPreferenceDTO {
+	
+	private String genre;
+	private Long likedTracksCount;
+	private Long listenedTracksCount;
+	
+	public WeightedPreferenceDTO() {}
+	
+	public WeightedPreferenceDTO(WeightedPreference pref) {
+		this.genre = pref.getGenre();
+		this.likedTracksCount = pref.getLikedTracksCount();
+		this.listenedTracksCount = pref.getListenedTracksCount();
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public Long getLikedTracksCount() {
+		return likedTracksCount;
+	}
+
+	public void setLikedTracksCount(Long likedTracksCount) {
+		this.likedTracksCount = likedTracksCount;
+	}
+
+	public Long getListenedTracksCount() {
+		return listenedTracksCount;
+	}
+
+	public void setListenedTracksCount(Long listenedTracksCount) {
+		this.listenedTracksCount = listenedTracksCount;
+	}
+	
+}
