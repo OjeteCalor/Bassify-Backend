c9840b21704174ca2f07a7f894c1266e7c6adb47
AntonioRG <ojetecalormetrica@gmail.com>
2025-06-05 15:07:15 +0200

    More tests added for the toolbox

--- src/test/java/es/metrica/Bassify_Backend/toolbox/TestToolbox.java
+++ src/test/java/es/metrica/Bassify_Backend/toolbox/TestToolbox.java
@@ -1,12 +1,21 @@
 package es.metrica.Bassify_Backend.toolbox;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.Properties;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.HttpClientErrorException;
+
+import es.metrica.Bassify_Backend.models.logic.toolbox.AccesToken;
 import es.metrica.Bassify_Backend.models.logic.toolbox.DeezerPreview;
+import es.metrica.Bassify_Backend.models.logic.toolbox.TokenPetition;
+import es.metrica.Bassify_Backend.properties.PropertiesSingleton;
 
 class TestToolbox {
 	
@@ -24,6 +33,35 @@ class TestToolbox {
 		assertEquals(DeezerPreview.getTrackPreview("Amador Rivas", "Mandanga Style").split("\\?")[0], 
 				"http://cdnt-preview.dzcdn.net/api/1/1/0/f/b/0/0fb9525af824a7b5db3a87fda130c4ea.mp3");
 		
+		assertEquals(DeezerPreview.getTrackPreview("Eminem", "Slim Shady").split("\\?")[0], 
+				DeezerPreview.getTrackPreview("Eminem", "Slim Shady").split("\\?")[0]);
+		
+		assertEquals(DeezerPreview.getTrackPreview("Tata Golosa", "Chiki Chiki").split("\\?")[0], 
+				DeezerPreview.getTrackPreview("Rodolfo Chikilcuatre", "Chiki Chiki").split("\\?")[0]);
+		
+		assertDoesNotThrow(() -> DeezerPreview.getTrackPreview("Artista Inexistente", "Cancion Inexistente"));
+		assertDoesNotThrow(() -> DeezerPreview.getTrackPreview("😀", "😀"));
+		
 		assertNull(DeezerPreview.getTrackPreview("Artista Inexistente", "Cancion Inexistente"));
+		assertNull(DeezerPreview.getTrackPreview("😀", "😀"));
+		
+		assertNotNull(DeezerPreview.getTrackPreview("", ""));
+		assertNotNull(DeezerPreview.getTrackPreview("Shit Infested Roach Human Carcass", "Shit Infested Roach Human Carcass"));
+	}
+	
+	@Test
+	void testRefreshToken() {
+		Properties p = PropertiesSingleton.getProperties();
+		
+		assertNotNull(AccesToken.getAccessToken(p.getProperty("refreshToken")));
+		assertThrows(HttpClientErrorException.class, () -> AccesToken.getAccessToken("no_valido"));
+	}
+	
+	@Test
+	void testAccessToken() {
+		Properties p = PropertiesSingleton.getProperties();
+		
+		assertNotNull(TokenPetition.getUserId(AccesToken.getAccessToken(p.getProperty("refreshToken"))));
+		assertThrows(NullPointerException.class, () -> TokenPetition.getUserId("no_valido"));
 	}
 }
