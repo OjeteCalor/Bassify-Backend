fc616723b16d45c112e80dbf582f2c7f4fc73c94
Gabriel <gabriel.gonzalez37h@gmail.com>
2025-06-05 11:22:41 +0200

    Añadir los generos a ArtistDTO

--- src/main/java/es/metrica/Bassify_Backend/models/dto/ArtistDTO.java
+++ src/main/java/es/metrica/Bassify_Backend/models/dto/ArtistDTO.java
@@ -1,16 +1,24 @@
 package es.metrica.Bassify_Backend.models.dto;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import es.metrica.Bassify_Backend.models.entity.Artist;
 
-public class ArtistDTO{
+public class ArtistDTO {
+	@JsonProperty("id")
 	private String artistSpotifyId;
+	@JsonProperty("name")
 	private String artistName;
+	private List<String> genres;
 	
 	public ArtistDTO() {}
 
 	public ArtistDTO(Artist artist) {
 		this.artistSpotifyId = artist.getArtistSpotifyId();
 		this.artistName = artist.getArtistName();
+		this.genres = artist.getArtistGenres();
 	}
 	
 	public String getArtistSpotifyId() {
@@ -24,6 +32,12 @@ public class ArtistDTO{
 	}
 	public void setArtistName(String artistName) {
 		this.artistName = artistName;
+	}
+	public List<String> getGenres() {
+		return genres;
+	}
+	public void setGenres(List<String> genres) {
+		this.genres = genres;
 	}
 	
 }
